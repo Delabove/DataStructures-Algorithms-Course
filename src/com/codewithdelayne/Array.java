d975fa018dcaa81915ff688c1b3475efc7f08aa7
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-05 23:39:20 -0500

    Practice set 1 of Array exercise

--- src/com/codewithdelayne/Array.java
+++ src/com/codewithdelayne/Array.java
@@ -1,6 +1,6 @@
 package com.codewithdelayne;
 
-
+import java.util.Arrays;
 
 public class Array {
     private int[] items;
@@ -10,33 +10,50 @@ public class Array {
         items = new int[length];
     }
 
-    //
-        public void insert(int item) {
-        //if full resize the array
+
+    public void insert(int item) {
         if (items.length == count) {
-            //resize array
-            int[] newItem = new int[count * 2];
-            //copy old array into new
-            for (int i = 0; i < count; i++) {
-                newItem[i] = items[i];
-                //set
-                items = newItem;
-//
-            }
+//            resize array
+            int[] newItems = new int[count * 2];
 
-
+//            copy old array into new
+            for (int i = 0; i < count; i++)
+                newItems[i] = items[i];
+//                set
+                items = newItems;
         }
-            //insert new item at the end
-            items[count++] = item;
+
+        //insert new item at the end
+        items[count++] = item;
     }
 
-            public void print () {
-                for (int i = 0; i < count; i++) {
-                    System.out.println(items[i]);
-                }
+
+    public void removeAt(int index){
+        //validate index boundaries
+        if(index < 0 || index >= count){
+            throw new IllegalArgumentException();
+        }
+        //remove index and shift everything over
+        for(int i = index; i < count; i++){
+            items[i] = items[i + 1];
+            count--;
+        }
+    }
+
+    public  int indexOf(int item) {
+
+        //look for it, if we find, return
+        int index = Arrays.binarySearch(items, item);
+        return (index < 0) ? -1 : index;
+    }
+
+
+
+    public void print() {
+            for (int i = 0; i < count; i++) {
+                System.out.println(items[i]);
             }
-
-
+        }
 
 
 }
--- src/com/codewithdelayne/Main.java
+++ src/com/codewithdelayne/Main.java
@@ -7,7 +7,11 @@ public class Main {
        Array numbers = new Array(3);
        numbers.insert(20);
        numbers.insert(30);
+       numbers.insert(40);
+       numbers.insert(50);
+       numbers.removeAt(2);
        numbers.print();
+        System.out.println(numbers.indexOf(100));
 
     }
 }
