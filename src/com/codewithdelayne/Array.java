2b2090965341325a8b15fcd86cd6846247b0177e
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-06 08:19:35 -0500

    Insert new items

--- src/com/codewithdelayne/Array.java
+++ src/com/codewithdelayne/Array.java
@@ -11,7 +11,27 @@ public class Array {
         subjects = new String[length];
     }
 
+//    Insert values
+    public void insert(String subject){
+        //check to see if the array is full
+        if(subjects.length == count){
+            //resize the array x 2
+            String[] newSubjectList = new String[count *2];
+            //copy old array into new array
+            for(int i = 0; i < count; i++)
+                newSubjectList[i] = subjects[i];
+            //set new array item
+            subjects = newSubjectList;
+        }
 
+
+
+
+
+
+        //add item to end
+         subjects[count++] = subject;
+    }
 
 
 
@@ -20,19 +40,6 @@ public class Array {
             System.out.println(subjects[i]);
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-//    Insert values
 //    Remove values
 //    Index of Values
 }
