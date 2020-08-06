879b8fdf1d3c1dcdcbafa0b27e7a02bf5094de40
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-05 19:48:38 -0500

    Crud -Create

--- src/com/codewithdelayne/Array.java
+++ src/com/codewithdelayne/Array.java
@@ -1,4 +1,12 @@
 package com.codewithdelayne;
 
+import java.util.List;
+
 public class Array {
+   private int[] items;
+    public Array(int length) {
+        items = new int[length];
+
+    }
+
 }
--- src/com/codewithdelayne/Main.java
+++ src/com/codewithdelayne/Main.java
@@ -5,9 +5,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = new int[3];
-        System.out.println(Arrays.toString(numbers));
-    }
+       Array items = new Array(3);
+        System.out.println(items);
 
     }
 }
