11fe96fbce27dad1cbe71d2258717c4de6b53bb1
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-06 08:49:23 -0500

    created class 4th repition

--- src/com/codewithdelayne/Array.java
+++ src/com/codewithdelayne/Array.java
@@ -3,55 +3,16 @@ package com.codewithdelayne;
 import java.util.Arrays;
 
 public class Array {
-//    Create Array
-    private String[] subjects;
+    private int[] numbers;
     private int count;
 
-    public Array(int length){
-        subjects = new String[length];
+    public Array(int length) {
+        numbers = new int[length];
     }
 
-//    Insert values
-    public void insert(String subject){
-        if(subjects.length == count){
-            String[] newSubjectList = new String[count *2];
-
-            for(int i = 0; i < count; i++)
-                newSubjectList[i] = subjects[i];
-
-            subjects = newSubjectList;
-        }
-
-         subjects[count++] = subject;
+    public void print() {
+        for (int i = 0; i < count; i++)
+            System.out.println(numbers[i]);
     }
 
-//    Remove values
-    public void removeAt(int index){
-//        validate boundaries
-        if(index < 0 || index > count){
-            throw new IllegalArgumentException();
-        }
-//        find index
-        for(int i = index; i < count; i++)
-//        shift everything to the left;
-            subjects[i] = subjects[i + 1];
-            count--;
-    }
-
-    public int findIndexOf(String subject){
-        for(int i = 0; i < count; i++){
-            subjects[i]= subject;
-            return i;
-        }
-        return -1;
-    }
-
-
-    public void print (){
-        for(int i = 0; i < count; i++){
-            System.out.println(subjects[i]);
-        }
-    }
-
-//    Index of Values
 }
--- src/com/codewithdelayne/Main.java
+++ src/com/codewithdelayne/Main.java
@@ -4,14 +4,8 @@ package com.codewithdelayne;
 public class Main {
 
     public static void main(String[] args) {
-        Array subjects = new Array(3);
-        subjects.insert("Math");
-        subjects.insert("Science");
-        subjects.insert("English");
-        subjects.insert("Social Studies");
-        subjects.removeAt(2);
-        subjects.print();
-        System.out.println(subjects.findIndexOf("Science"));
+       Array numbers = new Array(3);
 
+       numbers.print();
     }
 }
