9cab80ba4065e47131e714b48c78cdc951ae0a93
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-06 06:57:28 -0500

    Insert and test -CRUD

--- src/com/codewithdelayne/Array.java
+++ src/com/codewithdelayne/Array.java
@@ -8,10 +8,31 @@ public class Array {
     private int count;
 
     public Array(int length){
-        String [] cities = new String[length];
+        cities = new String[length];
     }
 
-    //INSERT DATA -UPDATE
+//    //INSERT DATA -UPDATE
+
+
+    public void insert(String city){
+
+//        //RESIZE ARRAY
+//        find out if current array is full
+        if(cities.length == count){
+            //if so, create a new one, and double size
+            String[] newCitiesList = new String[count * 2];
+            //copy old array into new array
+            for(int i = 0; i < count; i++)
+                newCitiesList[i] = cities[i];
+            //set
+            cities = newCitiesList;
+        }
+
+//insert new item at end
+            cities[count++] = city;
+    }
+
+
     //REMOVE -DELETE
     //INDEX
 
--- src/com/codewithdelayne/Main.java
+++ src/com/codewithdelayne/Main.java
@@ -5,6 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
        Array cities = new Array(3);
+       cities.insert("Dallas");
+       cities.insert("Lafayette");
+       cities.insert("Addison");
+       cities.insert("Arlington");
        cities.print();
 
     }
