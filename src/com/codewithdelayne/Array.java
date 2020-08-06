ae75f461691ff11c6fded2fe6d40737f386b0049
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-06 08:02:16 -0500

    Created class

--- src/com/codewithdelayne/Array.java
+++ src/com/codewithdelayne/Array.java
@@ -3,51 +3,36 @@ package com.codewithdelayne;
 import java.util.Arrays;
 
 public class Array {
-
-    private String[] cities;
+//    Create Array
+    private String[] subjects;
     private int count;
 
     public Array(int length){
-        cities = new String[length];
+        subjects = new String[length];
     }
 
-    public void insert(String city){
 
-        if(cities.length == count){
-            String[] newCitiesList = new String[count * 2];
 
-            for(int i = 0; i < count; i++)
-                newCitiesList[i] = cities[i];
 
-            cities = newCitiesList;
-        }
-            cities[count++] = city;
-    }
 
-    public void removeAt(int index){
-        if(index < 0 || index > count){
-            throw new IllegalArgumentException();
-        }
-        for(int i = index; i < count; i++){
-            cities[i] = cities[i + 1];
-            count--;
-        }
-    }
-
-    //INDEX
-    public int findIndex(String city){
+    public void print (){
         for(int i = 0; i < count; i++){
-            if(cities[i] == city){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public void print(){
-        for(int i = 0; i < count; i++) {
-            System.out.println(cities[i]);
+            System.out.println(subjects[i]);
         }
     }
 
+
+
+
+
+
+
+
+
+
+
+
+//    Insert values
+//    Remove values
+//    Index of Values
 }
--- src/com/codewithdelayne/Main.java
+++ src/com/codewithdelayne/Main.java
@@ -4,14 +4,8 @@ package com.codewithdelayne;
 public class Main {
 
     public static void main(String[] args) {
-       Array cities = new Array(3);
-       cities.insert("Dallas");
-       cities.insert("Lafayette");
-       cities.insert("Addison");
-       cities.insert("Arlington");
-       cities.removeAt(3);
-       cities.print();
-        System.out.println(cities.findIndex("Addison"));
+        Array subjects = new Array(5);
+        subjects.print();
 
     }
 }
