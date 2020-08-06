fa3f54fc1845cc4c8ee28cf56f025826748caf1a
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-05 23:55:22 -0500

    Practice 2-Crud

--- src/com/codewithdelayne/Array.java
+++ src/com/codewithdelayne/Array.java
@@ -3,57 +3,23 @@ package com.codewithdelayne;
 import java.util.Arrays;
 
 public class Array {
-    private int[] items;
+   //CREATE ARRAY -CREATE
+    private String[] cities;
     private int count;
 
-    public Array(int length) {
-        items = new int[length];
+    public Array(int length){
+        String [] cities = new String[length];
     }
 
-
-    public void insert(int item) {
-        if (items.length == count) {
-//            resize array
-            int[] newItems = new int[count * 2];
-
-//            copy old array into new
-            for (int i = 0; i < count; i++)
-                newItems[i] = items[i];
-//                set
-                items = newItems;
-        }
-
-        //insert new item at the end
-        items[count++] = item;
-    }
+    //INSERT DATA -UPDATE
+    //REMOVE -DELETE
+    //INDEX
 
 
-    public void removeAt(int index){
-        //validate index boundaries
-        if(index < 0 || index >= count){
-            throw new IllegalArgumentException();
-        }
-        //remove index and shift everything over
-        for(int i = index; i < count; i++){
-            items[i] = items[i + 1];
-            count--;
+    public void print(){
+        for(int i = 0; i < count; i++) {
+            System.out.println(cities[i]);
         }
     }
-
-    public  int indexOf(int item) {
-
-        //look for it, if we find, return
-        int index = Arrays.binarySearch(items, item);
-        return (index < 0) ? -1 : index;
-    }
-
-
-
-    public void print() {
-            for (int i = 0; i < count; i++) {
-                System.out.println(items[i]);
-            }
-        }
-
 
 }
--- src/com/codewithdelayne/Main.java
+++ src/com/codewithdelayne/Main.java
@@ -4,14 +4,8 @@ package com.codewithdelayne;
 public class Main {
 
     public static void main(String[] args) {
-       Array numbers = new Array(3);
-       numbers.insert(20);
-       numbers.insert(30);
-       numbers.insert(40);
-       numbers.insert(50);
-       numbers.removeAt(2);
-       numbers.print();
-        System.out.println(numbers.indexOf(100));
+       Array cities = new Array(3);
+       cities.print();
 
     }
 }
