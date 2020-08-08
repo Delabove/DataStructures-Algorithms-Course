cefdd5882545f394808a12f78f696a38d4ac7916
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-07 23:39:03 -0500

    For each to loop through and push character into stack and while loop that appends the popped characters into the stack as a string

--- src/com/codewithdelayne/StringReverser.java
+++ src/com/codewithdelayne/StringReverser.java
@@ -8,5 +8,11 @@ public class StringReverser {
         for(char ch : input.toCharArray()) {
             stack.push(ch);
         }
+
+        StringBuffer reversed = new StringBuffer();
+        while(!stack.empty())
+            reversed.append(stack.pop());
+
+        return reversed.toString();
     }
 }
