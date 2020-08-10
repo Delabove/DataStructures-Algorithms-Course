29b15dce8cf5ce0796719df385a1dcec7b7e18aa
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-10 14:47:42 -0500

    Returning updated max value

--- src/com/codewithdelayne/Array2D.java
+++ src/com/codewithdelayne/Array2D.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Array2D {
 
-    public int Array2D(int[][] arr){
+    public static int hourglass(int[][] arr) {
 
         //Max Sum
 
@@ -12,25 +12,37 @@ public class Array2D {
         //update
         //end when length has been reached and max has been found
 
-
         //start -current max
         int MAX_VALUE = -63;
+        int currentSum = 0;
 
         //loop through and get sum of each
-        for (int i = 0; i < 6; i++)//ROW
+        for (int i = 0; i <= 3; i++)//ROW
         {
-            for (int j = 0; j < 6; j++) //COLUMN
-            {  //compare current sum with start max value
+            for (int j = 0; j <= 3; j++) //COLUMN
+            {    //calculate sum//
+                currentSum = currentSum + arr[i][j];
+                System.out.print(arr[i][j] + " ");
 
+                //compare current sum with start max value
+                MAX_VALUE = Math.max(MAX_VALUE, currentSum);
 
 
             }
         }
         return MAX_VALUE;
+
     }
+
+
 
     public static void main(String[] args) {
 
+        int[][] arr =
+        hourglass(arr);
     }
+
+
 }
+
 
