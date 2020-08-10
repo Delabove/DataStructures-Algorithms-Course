caa559c51b0e3e3af3f2855b5ad351517b99405a
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-10 13:41:22 -0500

    Reworking code

--- src/com/codewithdelayne/Array2D.java
+++ src/com/codewithdelayne/Array2D.java
@@ -5,13 +5,24 @@ import java.util.Arrays;
 public class Array2D {
 
     public static void main(String[] args) {
+        //6x6 2d
+        //hourglass: 3x3
+            //traverse 3 times
+        //hourglass sum: arr[i]+arr[j]
+        //worst case: -63
+        //Max Sum
+            //start -current max
+                //loop through and get sum of each
+                //compare with start max value
+                //update
+            //end when length has been reached and max has been found
 
-        int[][] arr = { { 1, 1, 1, 0, 0, 0}, { 1,0,1,0,0,0 } };
+        int[][] arr = new int[6][6];
         int sumOfSums = 0;
+        System.out.println(sumOfSums);
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
                 sumOfSums += arr[i][j];
-                System.out.println(sumOfSums);
                 System.out.println("" + Arrays.toString(arr[i]) + Arrays.toString(arr[j]) + " ");
             }
         }
