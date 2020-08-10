1e56e89369ce0098aad0fe3aaa2b9690f3538eff
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-10 13:50:01 -0500

    Restructured for loop with initial starting value with sum of given hourglass

--- src/com/codewithdelayne/Array2D.java
+++ src/com/codewithdelayne/Array2D.java
@@ -5,32 +5,28 @@ import java.util.Arrays;
 public class Array2D {
 
     public static void main(String[] args) {
-        //6x6 2d
-        //hourglass: 3x3
-            //traverse 3 times
-        //hourglass sum: arr[i]+arr[j]
-        //worst case: -63
-        //Max Sum
-            //start -current max
-                //loop through and get sum of each
-                //compare with start max value
-                //update
-            //end when length has been reached and max has been found
 
-        int[][] arr = new int[6][6];
-        int sumOfSums = 0;
-        System.out.println(sumOfSums);
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
-                sumOfSums += arr[i][j];
-                System.out.println("" + Arrays.toString(arr[i]) + Arrays.toString(arr[j]) + " ");
+        //Max Sum
+
+        //compare with start max value
+        //update
+        //end when length has been reached and max has been found
+
+
+        //start -current max
+        int MAX_VALUE = -63;
+
+        //loop through and get sum of each
+        for (int i = 0; i < 6; i++);//ROW
+        {
+            for (int i = 0; i < 6; i++); //COLUMN
+            {
+
+
             }
         }
 
 
-
     }
-
-
-
 }
+
