b813f5b0bc1bfd59793c38d850d1a7af35d1b0d4
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-10 03:21:41 -0500

    Nested for loop for 2D array

--- src/com/codewithdelayne/Array2D.java
+++ src/com/codewithdelayne/Array2D.java
@@ -3,9 +3,20 @@ package com.codewithdelayne;
 public class Array2D {
 
     public Array2D(int[][] arr){
-     int sumOfi;
-     int sumOfj;
+     int sumOfi= 0;
+     int sumOfj = 0;
      int sumOfSums;
+
+
+     for(int i = 0; i < 6; i++){
+         for(int j = 0; j < 6; j++){
+
+
+         }
+     }
+
+
+
 
     }
 
